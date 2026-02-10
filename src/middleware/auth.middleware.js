06c77698296f83e1a14068d949a741453b092134
Gaurav Moghe <gauravmoghe0709@gmail.com>
2026-02-10 12:20:48 +0530

    refactor auth middleware for improved token validation and error handling

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,30 +4,27 @@ const usermodel = require ("../models/user.model")
 
 async function authmiddleware(req,res,next){
 
-    const {token} = req.cookies
+    const { token } = req.cookies
 
     if(!token){
         return res.status(401).json({
-            message: "invalid token.."
+            message: "invalid token"
         })
     }
 
     try {
-        
         const decode = jwt.verify(token, process.env.JWT_SECRATE)
-        console.log(decode)
 
         const user = await usermodel.findById(decode.id).select("-password")
         req.user = user
         next()
 
-
     } catch (error) {
-            console.log(error)        
+        return res.status(401).json({
+            message: "invalid token"
+        })
     }
-
-
-    
 }
+
 
 module.exports = authmiddleware
